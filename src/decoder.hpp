39b504c81f0eca7a4a337b221fe7ddd550b137e4
pl0q1n <pl0q1n@gmail.com>
2016-12-10 21:23:04 +0400

    first test added & godness iter was refactored. Also made code format

--- src/decoder.hpp
+++ src/decoder.hpp
@@ -13,142 +13,151 @@
 
 namespace lzw {
 
-namespace {
-// TODO: test it
-template <class RandomAccessIter, class T>
-RandomAccessIter find_unescaped(const RandomAccessIter begin,
-                                const RandomAccessIter end,
-                                const T &what_to_find) {
-  auto current = std::find(begin, end, what_to_find);
-  if (current == begin) {
-    return current;
-  }
+	// TODO: test it
+	template <class RandomAccessIter, class T>
+	RandomAccessIter find_unescaped(const RandomAccessIter begin,
+		const RandomAccessIter end,
+		const T &what_to_find) {
+		auto current = std::find(begin, end, what_to_find);
+		if (current == begin) {
+			return current;
+		}
 
-  while (current != end && *(current - 1) == '\\') {
-    current = std::find(current, end, what_to_find);
-  }
-  return current;
-}
+		while (current != end && *(current - 1) == '\\') {
+			current = std::find(current, end, what_to_find);
+		}
+		return current;
+	}
 
-template <class Iter> class UnescapeIterator {
-  Iter _inner;
+	template <class Iter> class UnescapeIterator : public std::iterator<std::forward_iterator_tag, char>
+	{
+		Iter _inner;
 
-public:
-  UnescapeIterator(Iter it) : _inner(it) {}
-  UnescapeIterator(const UnescapeIterator &) = default;
-  UnescapeIterator(UnescapeIterator &&) = default;
-  UnescapeIterator &operator=(const UnescapeIterator &) = default;
-  UnescapeIterator &operator=(UnescapeIterator &&) = default;
-  ~UnescapeIterator() = default;
+		//state, that show us, do we did a skip at previous dereference
+		bool _skip_state = false;
 
-  UnescapeIterator operator++() {
-    ++_inner;
-    return *this;
-  }
+	public:
+		UnescapeIterator(Iter it) : _inner(it) {}
+		UnescapeIterator(const UnescapeIterator &) = default;
+		UnescapeIterator(UnescapeIterator &&) = default;
+		UnescapeIterator &operator=(const UnescapeIterator &) = default;
+		UnescapeIterator &operator=(UnescapeIterator &&) = default;
+		~UnescapeIterator() = default;
 
-  /* typename std::iterator_traits<Iter>::value_type */
-  char operator*() {
-    while (*_inner == '\\') {
-      ++_inner;
-    }
-    return *_inner;
-  }
+		UnescapeIterator operator++() {
+			++_inner;
+			return *this;
+		}
 
-  bool operator!=(const UnescapeIterator &rhs) { return _inner != rhs._inner; }
-};
+		/* typename std::iterator_traits<Iter>::value_type */
+		char operator*() {
+			if (_skip_state == false && *_inner == '\\') {
+				++_inner;
+				_skip_state = true;
+			}
+			else {
+				_skip_state = false;
+			}
+			return *_inner;
+		}
 
-template <class Iter> UnescapeIterator<Iter> unescape(Iter &&it) {
-  return UnescapeIterator<Iter>(std::forward<Iter>(it));
-}
+		bool operator!=(const UnescapeIterator &rhs) { return _inner != rhs._inner; }
+		bool operator==(const UnescapeIterator &rhs) { return !(*this != rhs); }
+	};
 
-} // anonymous namespace
+	template <class Iter> UnescapeIterator<Iter> unescape(Iter &&it) {
+		return UnescapeIterator<Iter>(std::forward<Iter>(it));
+	}
 
-template <class Value> class decoder {
-  // wanted to use std::vector<char>, but this structure doesn't has the hash
-  // function in unordered map :(
-  using map_t = std::vector<Value>;
 
-private:
-  map_t _map;
+	template <class Value> class decoder {
+		// wanted to use std::vector<char>, but this structure doesn't has the hash
+		// function in unordered map :(
+		using map_t = std::vector<Value>;
 
-public:
-  decoder() {
-    for (auto i = 0; i < 0xff; i++) {
-      _map.emplace_back(1, i);
-    }
-    assert(_map.size() == 0xff);
-  }
+	private:
+		map_t _map;
 
-  decoder(const decoder &object) = default;
-  decoder(decoder &&object) = default;
-  decoder &operator=(const decoder &object) = default;
-  decoder &operator=(decoder &&object) = default;
-  ~decoder() = default;
+	public:
+		decoder() {
+			for (auto i = 0; i < 0xff; i++) {
+				_map.emplace_back(1, i);
+			}
+			assert(_map.size() == 0xff);
+		}
 
-  // TODO: refactor this
-  template <class InputIter>
-  std::vector<char> decode(InputIter begin, InputIter end) {
-    uint16_t current_code = *begin;
-    uint16_t previous_code = current_code;
-    Value previous;
-    Value current;
-    assert(current_code < _map.size());
-    std::vector<char> decoded = {
-        _map[current_code][0]}; // main output -- decoded file
+		decoder(const decoder &object) = default;
+		decoder(decoder &&object) = default;
+		decoder &operator=(const decoder &object) = default;
+		decoder &operator=(decoder &&object) = default;
+		~decoder() = default;
 
-    ++begin;
-    while (begin != end) {
-      current_code = *begin;
-      if (current_code < _map.size()) {
-        const auto &temp = _map[current_code];
-        decoded.insert(decoded.end(), temp.begin(), temp.end());
-        assert(previous_code < _map.size());
-        previous = _map[previous_code];
-        current = std::string(1, temp[0]);
-        _map.emplace_back(previous + current);
-      } else {
-        assert(current_code == _map.size());
-        assert(previous_code < _map.size());
-        previous = _map[previous_code];
-        current = std::string(1, _map[previous_code][0]);
-        decoded.insert(decoded.end(), previous.begin(), previous.end());
-        decoded.emplace_back(current[0]);
-      }
-      previous_code = current_code;
-      ++begin;
-    }
-    return decoded;
-  }
+		// TODO: refactor this
+		template <class InputIter>
+		std::vector<char> decode(InputIter begin, InputIter end) {
+			uint16_t current_code = *begin;
+			uint16_t previous_code = current_code;
+			Value previous;
+			Value current;
+			assert(current_code < _map.size());
+			std::vector<char> decoded = {
+				_map[current_code][0] }; // main output -- decoded file
 
-  template <class RandomAccessIter>
-  static std::vector<char> parallel_decode(const RandomAccessIter begin,
-                                           const RandomAccessIter end) {
-    using future_t = std::future<std::vector<char>>;
-    std::vector<future_t> futures;
+			++begin;
+			while (begin != end) {
+				current_code = *begin;
+				if (current_code < _map.size()) {
+					const auto &temp = _map[current_code];
+					decoded.insert(decoded.end(), temp.begin(), temp.end());
+					assert(previous_code < _map.size());
+					previous = _map[previous_code];
+					current = std::string(1, temp[0]);
+					_map.emplace_back(previous + current);
+				}
+				else {
+					assert(current_code == _map.size());
+					assert(previous_code < _map.size());
+					previous = _map[previous_code];
+					current = std::string(1, _map[previous_code][0]);
+					decoded.insert(decoded.end(), previous.begin(), previous.end());
+					decoded.emplace_back(current[0]);
+				}
+				previous_code = current_code;
+				++begin;
+			}
+			return decoded;
+		}
 
-    auto current = begin;
-    auto last = find_unescaped(current, end, '\n');
-    while (last != end) {
-      futures.emplace_back(
-          std::async([ from = unescape(current), to = unescape(last) ]() {
-            decoder local_decoder;
-            return local_decoder.decode(from, to);
-          }));
-      current = last + 1;
-      last = find_unescaped(current, end, '\n');
-    }
+		template <class RandomAccessIter>
+		static std::vector<char> parallel_decode(const RandomAccessIter begin,
+			const RandomAccessIter end) {
+			using future_t = std::future<std::vector<char>>;
+			std::vector<future_t> futures;
 
-    std::vector<char> decoded;
-    for (auto &future : futures) {
-      if (future.valid()) {
-        const auto &data = future.get();
-        decoded.insert(decoded.end(), data.begin(), data.end());
-      } else {
-        throw std::runtime_error("Something going bad.");
-      }
-    }
-    return decoded;
-  }
-};
+			auto current = begin;
+			auto last = find_unescaped(current, end, '\n');
+			while (last != end) {
+				futures.emplace_back(
+					std::async([from = unescape(current), to = unescape(last)]() {
+					decoder local_decoder;
+					return local_decoder.decode(from, to);
+				}));
+				current = last + 1;
+				last = find_unescaped(current, end, '\n');
+			}
+
+			std::vector<char> decoded;
+			for (auto &future : futures) {
+				if (future.valid()) {
+					const auto &data = future.get();
+					decoded.insert(decoded.end(), data.begin(), data.end());
+				}
+				else {
+					throw std::runtime_error("Something going bad.");
+				}
+			}
+			return decoded;
+		}
+	};
 }
 #endif // DECODER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -13,59 +13,71 @@ using encoded_data_t = lzw::encoder<char>::parallel_encoded_data_t;
 
 template <class OutputStream>
 void write_to_stream(OutputStream &ostream, encoded_data_t encoded) {
-  for (const auto &encoded_part : encoded) {
-    const char *const begin =
-        reinterpret_cast<const char *>(encoded_part.data());
-    const size_t size = sizeof(encoded_part[0]) * encoded_part.size();
-    const char *const end = begin + size;
+	for (const auto &encoded_part : encoded) {
+		const char *const begin =
+			reinterpret_cast<const char *>(encoded_part.data());
+		const size_t size = sizeof(encoded_part[0]) * encoded_part.size();
+		const char *const end = begin + size;
 
-    for (const char *current = begin; current != end; ++current) {
-      if (*current == '\n' || *current == '\\') {
-        io::put(ostream, '\\');
-      }
-      io::put(ostream, *current);
-    }
-    io::put(ostream, '\n');
-  }
+		for (const char *current = begin; current != end; ++current) {
+			if (*current == '\n' || *current == '\\') {
+				io::put(ostream, '\\');
+			}
+			io::put(ostream, *current);
+		}
+		io::put(ostream, '\n');
+	}
 }
 
 void decode(const std::string &path) {
-  io::mapped_file_source istream(path);
-  auto decoded = decoder_t::parallel_decode(istream.data(),
-                                            istream.data() + istream.size());
-  std::cout.write(decoded.data(), static_cast<std::streamsize>(decoded.size()));
+	io::mapped_file_source istream(path);
+	auto decoded = decoder_t::parallel_decode(istream.data(),
+		istream.data() + istream.size());
+	std::cout.write(decoded.data(), static_cast<std::streamsize>(decoded.size()));
 }
 
 void encode(const std::string &path) {
-  io::mapped_file_source istream(path);
-  auto encoded = encoder_t::parallel_encode(istream.data(),
-                                            istream.data() + istream.size());
-  write_to_stream(std::cout, encoded);
+	io::mapped_file_source istream(path);
+	auto encoded = encoder_t::parallel_encode(istream.data(),
+		istream.data() + istream.size());
+	write_to_stream(std::cout, encoded);
+}
+
+void test() {
+	std::string input = "\\\\ \\\n \\ \n";
+	std::string expected = "\\ \n  \n";
+	auto first = lzw::unescape(input.begin());
+	auto last = lzw::unescape(input.end());
+	assert(std::equal(first, last, expected.begin(), expected.end()));
 }
 
 int main(int argc, char *argv[]) {
-  if (argc != 3) {
-    std::cout << "Use args, Luke! \nUsage: <what_to_do> <file_path> where "
-                 "what_to_do in {'-d', '-e'})"
-              << std::endl;
-    exit(0);
-  }
+	test();
+	if (argc != 3) {
+		std::cout << "Use args, Luke! \nUsage: <what_to_do> <file_path> where "
+			"what_to_do in {'-d', '-e'})"
+			<< std::endl;
+		exit(0);
+	}
 
-  const auto what_to_do = std::string(argv[1]);
-  const auto file_path = std::string(argv[2]);
+	const auto what_to_do = std::string(argv[1]);
+	const auto file_path = std::string(argv[2]);
 
-  try {
-    if (what_to_do == "-d") {
-      decode(file_path);
-    } else if (what_to_do == "-e") {
-      encode(file_path);
-    } else {
-      std::cout << "Incorrect option. Use -d or -e." << std::endl;
-      std::exit(1);
-    }
-  } catch (const std::exception &e) {
-    std::cout << "Error: " << e.what() << std::endl;
-  }
+	try {
+		if (what_to_do == "-d") {
+			decode(file_path);
+		}
+		else if (what_to_do == "-e") {
+			encode(file_path);
+		}
+		else {
+			std::cout << "Incorrect option. Use -d or -e." << std::endl;
+			std::exit(1);
+		}
+	}
+	catch (const std::exception &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
 
-  return 0;
+	return 0;
 }
