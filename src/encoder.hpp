6816f549bac5a0c984dfc45656a5ba51c45b2fac
pl0q1n <pl0q1n@gmail.com>
2016-12-11 04:01:21 +0400

    some serious architectural changes & fixes

--- src/encoder.hpp
+++ src/encoder.hpp
@@ -7,92 +7,92 @@
 #include <vector>
 
 namespace lzw {
-	template <class Key> class encoder {
-		// wanted to use std::vector<char>, but this structure doesn't has the hash
-		// function in unordered map :(
-		using map_t = std::unordered_map<Key, uint16_t>;
-		using data_t = std::vector<uint16_t>;
+template <class Key> class encoder {
+  // wanted to use std::vector<char>, but this structure doesn't has the hash
+  // function in unordered map :(
+  using map_t = std::unordered_map<Key, uint16_t>;
+  using data_t = std::vector<uint16_t>;
 
-		map_t _map;
+  map_t _map;
 
-	public:
-		using parallel_encoded_data_t = std::vector<data_t>;
+public:
+  using parallel_encoded_data_t = std::vector<data_t>;
 
-		encoder() {
-			for (int i = 0; i < 0xff; i++) {
-				_map.emplace(std::string(1, static_cast<char>(i)), i);
-			}
-		}
+  encoder() {
+    for (int i = 0; i <= 0xff; i++) {
+      _map.emplace(std::string(1, static_cast<char>(i)), i);
+    }
+  }
 
-		encoder(const encoder &object) = default;
-		encoder(encoder &&object) = default;
-		encoder &operator=(const encoder &object) = default;
-		encoder &operator=(encoder &&object) = default;
-		~encoder() = default;
+  encoder(const encoder &object) = default;
+  encoder(encoder &&object) = default;
+  encoder &operator=(const encoder &object) = default;
+  encoder &operator=(encoder &&object) = default;
+  ~encoder() = default;
 
-		template <class InputIter> data_t encode(InputIter begin, InputIter end) {
-			// init block
-			Key previous;   // previous character/characters
-			Key current;    // current character/characters
-			data_t encoded; // main output -- encoded string
+  template <class InputIter> data_t encode(InputIter begin, InputIter end) {
+    // init block
+    Key previous;   // previous character/characters
+    Key current;    // current character/characters
+    data_t encoded; // main output -- encoded string
 
-			// main cycle
-			while (begin != end) {
-				current = *begin;
-				if (_map.find(previous + current) !=
-					_map.end()) { // if we find pair of character in hashtable
-					previous += current;
-				}
-				else { // if we don't find
-					auto search = _map.find(previous);
-					encoded.emplace_back(search->second);
-					_map.emplace(previous + current, _map.size());
-					previous = current;
-				}
-				++begin;
-			}
-			auto search = _map.find(current);
-			encoded.emplace_back(search->second);
-			return encoded;
-		}
+    // main cycle
+    while (begin != end) {
+      current = *begin;
+      if (_map.find(previous + current) !=
+          _map.end()) { // if we find pair of character in hashtable
+        previous += current;
+      } else { // if we don't find
+        auto search = _map.find(previous);
+        assert(search != _map.end());
+        encoded.emplace_back(search->second);
+        _map.emplace(previous + current, _map.size());
+        previous = current;
+      }
+      ++begin;
+    }
+    auto search = _map.find(current);
+    assert(search != _map.end());
+    encoded.emplace_back(search->second);
+    return encoded;
+  }
 
-		template <class RandomAccessIter>
-		static std::vector<data_t> parallel_encode(const RandomAccessIter begin,
-			const RandomAccessIter end) {
-			using future_t = std::future<data_t>;
-			const auto size = end - begin;
+  template <class RandomAccessIter>
+  static std::vector<data_t> parallel_encode(const RandomAccessIter begin,
+                                             const RandomAccessIter end) {
+    using future_t = std::future<data_t>;
+    const auto size = end - begin;
 
-			// FIXME: no need for n threads if size is small
-			// FIXME: if size < nthreads then size_per_thread = 0
-			const auto nthreads = std::thread::hardware_concurrency();
-			const auto size_per_thread = size / nthreads;
+    // FIXME: no need for n threads if size is small
+    // FIXME: if size < nthreads then size_per_thread = 0
+    const auto nthreads = size < 1024 ? 1 : std::thread::hardware_concurrency();
+    const auto size_per_thread = size / nthreads;
 
-			std::vector<future_t> futures;
-			for (unsigned int i = 0; i < nthreads - 1; i++) {
-				futures.emplace_back(std::async(
-					[start = begin + i * size_per_thread, size_per_thread]() {
-					encoder local_encoder;
-					return local_encoder.encode(start, start + size_per_thread);
-				}));
-			}
+    std::vector<future_t> futures;
+    for (unsigned int i = 0; i < nthreads - 1; i++) {
+      futures.emplace_back(std::async(
+          [ start = begin + i * size_per_thread, size_per_thread ]() {
+            encoder local_encoder;
+            return local_encoder.encode(start, start + size_per_thread);
+          }));
+    }
 
-			futures.emplace_back(
-				std::async([start = begin + (nthreads - 1) * size_per_thread, end]() {
-				encoder local_encoder;
-				return local_encoder.encode(start, end);
-			}));
-
-			std::vector<data_t> encoded_data(nthreads);
-			for (auto &&future : futures) {
-				if (future.valid()) {
-					encoded_data.emplace_back(future.get());
-				}
-				else {
-					throw std::runtime_error("Something going wrong.");
-				}
-			}
-			return encoded_data;
-		}
-	};
+    futures.emplace_back(
+        std::async([ start = begin + (nthreads - 1) * size_per_thread, end ]() {
+          encoder local_encoder;
+          return local_encoder.encode(start, end);
+        }));
+    std::cout << "emplace suxsex" << std::endl;
+    std::vector<data_t> encoded_data(nthreads);
+    for (auto &&future : futures) {
+      if (future.valid()) {
+        encoded_data.emplace_back(future.get());
+      } else {
+        throw std::runtime_error("Something going wrong.");
+      }
+    }
+    return encoded_data;
+  }
+};
 }
 #endif // ENCODER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -13,71 +13,107 @@ using encoded_data_t = lzw::encoder<char>::parallel_encoded_data_t;
 
 template <class OutputStream>
 void write_to_stream(OutputStream &ostream, encoded_data_t encoded) {
-	for (const auto &encoded_part : encoded) {
-		const char *const begin =
-			reinterpret_cast<const char *>(encoded_part.data());
-		const size_t size = sizeof(encoded_part[0]) * encoded_part.size();
-		const char *const end = begin + size;
+  for (const auto &encoded_part : encoded) {
+    const char *const begin =
+        reinterpret_cast<const char *>(encoded_part.data());
+    const size_t size = sizeof(encoded_part[0]) * encoded_part.size();
+    const char *const end = begin + size;
 
-		for (const char *current = begin; current != end; ++current) {
-			if (*current == '\n' || *current == '\\') {
-				io::put(ostream, '\\');
-			}
-			io::put(ostream, *current);
-		}
-		io::put(ostream, '\n');
-	}
+    for (const char *current = begin; current != end; ++current) {
+      if (*current == '\n' || *current == '\\') {
+        io::put(ostream, '\\');
+      }
+      io::put(ostream, *current);
+    }
+    io::put(ostream, '\n');
+  }
 }
 
 void decode(const std::string &path) {
-	io::mapped_file_source istream(path);
-	auto decoded = decoder_t::parallel_decode(istream.data(),
-		istream.data() + istream.size());
-	std::cout.write(decoded.data(), static_cast<std::streamsize>(decoded.size()));
+  io::mapped_file_source istream(path);
+  auto decoded = decoder_t::parallel_decode(
+      reinterpret_cast<const uint16_t *>(istream.data()),
+      reinterpret_cast<const uint16_t *>(istream.data()) +
+          istream.size() / sizeof(uint16_t));
+  std::cout.write(decoded.data(), static_cast<std::streamsize>(decoded.size()));
 }
 
 void encode(const std::string &path) {
-	io::mapped_file_source istream(path);
-	auto encoded = encoder_t::parallel_encode(istream.data(),
-		istream.data() + istream.size());
-	write_to_stream(std::cout, encoded);
+  io::mapped_file_source istream(path);
+  auto encoded = encoder_t::parallel_encode(istream.data(),
+                                            istream.data() + istream.size());
+  write_to_stream(std::cout, encoded);
+}
+// TODO: istream -> file
+void encode_single(const std::string &path) {
+  io::mapped_file_source istream(path);
+  auto encoder = encoder_t();
+  auto encoded =
+      encoder.encode(istream.data(), istream.data() + istream.size());
+  std::cout.write(reinterpret_cast<const char *>(encoded.data()),
+                  encoded.size() * sizeof(encoded[0]));
 }
 
-void test() {
-	std::string input = "\\\\ \\\n \\ \n";
-	std::string expected = "\\ \n  \n";
-	auto first = lzw::unescape(input.begin());
-	auto last = lzw::unescape(input.end());
-	assert(std::equal(first, last, expected.begin(), expected.end()));
+void decode_single(const std::string &path) {
+  io::mapped_file_source istream(path);
+  auto decoder = decoder_t();
+  auto decoded =
+      decoder.decode(reinterpret_cast<const uint16_t *>(istream.data()),
+                     reinterpret_cast<const uint16_t *>(istream.data()) +
+                         istream.size() / sizeof(uint16_t));
+  std::cout.write(decoded.data(), static_cast<std::streamsize>(decoded.size()));
+}
+
+void iter_test() {
+  std::string input = "\\\\ \\\n \\ \n";
+  std::string expected = "\\ \n  \n";
+  auto first = lzw::unescape(input.begin());
+  auto last = lzw::unescape(input.end());
+  assert(std::equal(first, last, expected.begin(), expected.end()));
+}
+
+void decode_test() {
+  std::string input = "ENCODE ME PLS)00)";
+  auto encoder = encoder_t();
+  auto encoded = encoder.encode(input.begin(), input.end());
+  auto decoder = decoder_t();
+  auto decoded = decoder.decode(encoded.begin(), encoded.end());
+  assert(input == std::string(decoded.begin(), decoded.end()));
 }
 
 int main(int argc, char *argv[]) {
-	test();
-	if (argc != 3) {
-		std::cout << "Use args, Luke! \nUsage: <what_to_do> <file_path> where "
-			"what_to_do in {'-d', '-e'})"
-			<< std::endl;
-		exit(0);
-	}
+  // TODO: WARNING!!!! REMOVE DAMN TEMPLATES LEEEEEEEEEEL
+  iter_test();
+  decode_test();
+  // TODO: Replace this with boost library that make the same shit
+  if (argc != 3) {
+    std::cout << "Use args, Luke! \nUsage: <what_to_do> <file_path> where "
+                 "what_to_do in {'-d', '-e', '-ds', '-es'})"
+              << std::endl;
+    exit(0);
+  }
 
-	const auto what_to_do = std::string(argv[1]);
-	const auto file_path = std::string(argv[2]);
+  const auto what_to_do = std::string(argv[1]);
+  const auto file_path = std::string(argv[2]);
 
-	try {
-		if (what_to_do == "-d") {
-			decode(file_path);
-		}
-		else if (what_to_do == "-e") {
-			encode(file_path);
-		}
-		else {
-			std::cout << "Incorrect option. Use -d or -e." << std::endl;
-			std::exit(1);
-		}
-	}
-	catch (const std::exception &e) {
-		std::cout << "Error: " << e.what() << std::endl;
-	}
+  try {
+    if (what_to_do == "-d") {
+      decode(file_path);
+    } else if (what_to_do == "-e") {
+      encode(file_path);
+    } else if (what_to_do == "-ds") {
+      decode_single(file_path);
+    } else if (what_to_do == "-es") {
+      encode_single(file_path);
+    } else {
+      std::cout << "Incorrect option. Use -d or -e." << std::endl;
+      std::exit(1);
+    }
+  }
 
-	return 0;
+  catch (const std::exception &e) {
+    std::cout << "Error: " << e.what() << std::endl;
+  }
+
+  return 0;
 }
